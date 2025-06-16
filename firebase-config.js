a8734b1336c8ddfc4b7ace1c3998b1f82605a924
mahmoud778899 <mhoda8952@gmail.com>
2025-06-17 00:58:35 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// ملف إعدادات Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBGTqowfwQGfjwhajWBY44GZcpTVoC1VJA",
+  authDomain: "waffarnalak.firebaseapp.com",
+  projectId: "waffarnalak",
+
+  appId: ":921465813171:android:2c495b8f30aac8d78cd8c6"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
